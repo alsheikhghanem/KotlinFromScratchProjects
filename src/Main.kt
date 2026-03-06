6330d0ec867e6b2afeeffee703aaffbeac813ed9
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-03-06 05:03:41 +0300

    new exercise 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,3 @@
 fun main() {
 
-    val row: Int = 3
-    val column: Int = 4
-    val twoDimensionArray: Array<Array<Int>> = Array<Array<Int>>(row) { Array<Int>(column) { 0 } }
-    twoDimensionArray[0][0] = 14
-    twoDimensionArray[0][1] = 15
-    twoDimensionArray[0][2] = 16
-    twoDimensionArray.forEach { a ->
-        a.forEach { b ->
-            println("a:$a, b:$b")
-        }
-    }
-    twoDimensionArray[0][3] = 99
 }
