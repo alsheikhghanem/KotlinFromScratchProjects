6cb3c849e3ec28d2b004b7fafea548b4b3d9491b
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-03-06 05:01:43 +0300

    Code Snippet 23

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,15 @@
 fun main() {
 
-    val num = Array(4, {i -> i * 2})
-    var array = Array(4, { "$it" })
-    val element = array[1]
-    println(element)
-    array[2] = "Update"
-    println(array[2])
-    val size = array.size
-    println(size)
-    for (element in element) println(element)
-    val found = array.contains(element)
-    println(found)
-    val subArray = array.slice(0 .. 3)
-    subArray.forEach { println(it) }
-    array.sort()
-    array.joinToString()
+    val row: Int = 3
+    val column: Int = 4
+    val twoDimensionArray: Array<Array<Int>> = Array<Array<Int>>(row) { Array<Int>(column) { 0 } }
+    twoDimensionArray[0][0] = 14
+    twoDimensionArray[0][1] = 15
+    twoDimensionArray[0][2] = 16
+    twoDimensionArray.forEach { a ->
+        a.forEach { b ->
+            println("a:$a, b:$b")
+        }
+    }
+    twoDimensionArray[0][3] = 99
 }
