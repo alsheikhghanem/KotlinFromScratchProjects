9080ce64bd2265fc0f42544798ee99416357f70e
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-03-06 04:23:56 +0300

    Code Snippet 21

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
 
+    val arrInt: Array<Int> = arrayOf<Int>(10, 20, 30, 40)
+    println(arrInt[3])
+    val arrString: Array<String> = arrayOf<String>("One", "Two", "Three", "Four", "Five")
+    println(arrString[0])
+    val myArray: Array<Any> = arrayOf(1, "bye", false)
+
+    val charArray: CharArray = charArrayOf('a', 'b', 'c')
+
 }
